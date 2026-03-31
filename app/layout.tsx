43c95d012115cd2670a3c7064937411544786ad8
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-03-31 20:03:20 +0500

    navbar boshqa papkaga otkazildi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,25 @@
-'use client'
-
-import type { Metadata } from 'next'
 import { Fira_Code } from 'next/font/google'
 import './globals.css'
 import { ReactNode, use } from 'react'
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+import { Metadata } from 'next'
+import Navbar from '@/component/navbar'
 
-const firaCode = Fira_Code({
-    variable: '--font-fira-code',
-    subsets: ['latin'],
-    weight: '400',
-})
+const fira_code = Fira_Code({ subsets: ['latin'], weight: '400' })
+
+export const metadata: Metadata = {
+    title: 'Next.js 13.4 App Router',
+    description: 'Next.js 13.4 App Router bilan yaratilgan dastur',
+}
 
 export default function RootLayout({
     children,
 }: Readonly<{
     children: ReactNode
 }>) {
-    const router = useRouter()
     return (
-        <html lang='uz' className={`${firaCode.variable} ${firaCode.variable} h-full antialiased`}>
+        <html lang='uz' className={fira_code.className}>
             <body className='min-h-full flex flex-col'>
-                <div className='navbar-1'>
-                    <button onClick={() => router.push('/')}>Bosh sahifa</button>
-                    <button onClick={() => router.push('/about')}>Men haqimda</button>
-                    <button onClick={() => router.push('/contact')}>Bog'lanish</button>
-                </div>
-                {/* <div className='navbar'>
-                    <Link href='/'>Home</Link>
-                    <Link href='/about'>Men haqimda</Link>
-                    <Link href='/contact'>Bog'lanish</Link>
-                </div> */}
+                <Navbar />
                 {children}
                 <p>Footer</p>
             </body>
---
+++ component/navbar.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+export default function Navbar() {
+    const router = useRouter()
+
+    return (
+        <div className='navbar-1'>
+            <button onClick={() => router.push('/')}>Bosh sahifa</button>
+            <button onClick={() => router.push('/about')}>Men haqimda</button>
+            <button onClick={() => router.push('/contact')}>Bog'lanish</button>
+        </div>
+    )
+}

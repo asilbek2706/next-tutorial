6b28d9fb1ca63b980b8d5427cb47c3c9641f3859
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-04-03 13:36:27 +0500

    server loading explanation

---
+++ app/posts/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPost() {
+    return (
+        <div>
+            <h1>Not Found Posts</h1>
+        </div>
+    )
+}
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,15 +1,26 @@
 import { PostType } from '@/interface'
 import axios from 'axios'
 import Link from 'next/link'
+import { notFound } from 'next/navigation'
 
 async function getData() {
     const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+
+    // await new Promise((resolve) =>
+    //     setTimeout(() => {
+    //         resolve('')
+    //     }, 2000)
+    // )
 
     return data
 }
 
 const PostsPage = async () => {
     const data: PostType[] = await getData()
+
+    if (!data.length) {
+        notFound()
+    }
 
     return (
         <>
